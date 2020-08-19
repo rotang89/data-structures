40c384c4a6085bbce13086faaa152d1f66378fb5
chrisalopez1337 <chrisalopez@outlook.com>
2020-08-19 11:56:05 -0700

    Passes all but two tests, the skeleton is good going to come back to refactor later

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.key = 0;
+  this.store = {};
 };
+
+Stack.prototype.push = function(n) {
+  this.store[this.key] = n;
+  this.key++;
+}
+
+Stack.prototype.pop = function() {
+  this.key--;
+  if (this.key > 0) {
+    this.key = 0;
+  }
+  return this.store[this.key];
+}
+
+Stack.prototype.size = function() {
+  return this.key;
+}
 
 
