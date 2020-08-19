5c391f03f92c2b9e810f1679f2fe3d80a5ac07da
chrisalopez1337 <chrisalopez@outlook.com>
2020-08-19 11:59:49 -0700

    Added skeleton that passes most tests, going to refactor

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.key = 0;
+    this.store = {};
   }
 
+  push(n) {
+    this.store[this.key] = n;
+    this.key--;
+  }
+
+  pop() {
+    this.key--;
+    if (this.key > 0) {
+      this.key = 0;
+    }
+    return this.store[this.key];
+  }
+
+  size() {
+    return this.key;
+  }
 }
