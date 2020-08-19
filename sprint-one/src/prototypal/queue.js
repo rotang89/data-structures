29e3a5922f68fa92a7b3db5769a615e2fa4bb5e3
chrisalopez1337 <chrisalopez@outlook.com>
2020-08-19 15:46:45 -0700

    Added working prototypal instance of Queue(), passes all tests

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var que = Object.create(queueMethods);
+  que.key = 0;
+  que.i = 0;
+  que.storage = {};
+  return que;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue(value) {
+    this.i++;
+    this.storage[this.key] = value;
+    this.key++;
+  },
+  dequeue() {
+    this.i--;
+    if (this.i < 0) {
+      this.i = 0;
+    }
+    var least = Infinity;
+    for (var key in this.storage) {
+      if (key < least) {
+        least = key;
+      }
+    }
+    var string = this.storage[least];
+    delete this.storage[least];
+    return string;
+  },
+  size() {
+    return this.i;
+  }
+};
 
 
