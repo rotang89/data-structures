adcf3b93a0971156fd36111d685cdac26f218804
chrisalopez1337 <chrisalopez@outlook.com>
2020-08-19 15:31:31 -0700

    Adding working implentation for Queue() in the functional approach, passes all tests.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,18 +2,42 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  someInstance.storage = {};
+  someInstance.key = 0;
+  someInstance.i = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    someInstance.i++;
+    someInstance.storage[someInstance.key] = value;
+    someInstance.key++;
   };
 
   someInstance.dequeue = function() {
+    someInstance.i--;
+    if (someInstance.i < 0) {
+      someInstance.i = 0;
+    }
+    var least = Infinity;
+    for (var key in someInstance.storage) {
+      if (key < least) {
+        console.log('test');
+        least = key;
+      }
+    }
+    var string = someInstance.storage[least];
+    delete someInstance.storage[least];
+    return string;
   };
 
   someInstance.size = function() {
+    return someInstance.i;
   };
 
   return someInstance;
 };
+
+// [NEW] [] [] [] [] [REMOVE]
+// n^length ----------> 0 (numeric keys)
+//
