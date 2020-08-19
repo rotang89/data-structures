76a39bdcfc6b062a10b7de329347e4e8731dd6a7
chrisalopez1337 <chrisalopez@outlook.com>
2020-08-19 11:45:59 -0700

    Adding functional case for Stack, passes all test cases

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,24 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
-
+  someInstance.storage = {};
+  someInstance.key = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    someInstance.storage[someInstance.key] = value;
+    someInstance.key++;
   };
 
   someInstance.pop = function() {
+    someInstance.key--;
+    if(someInstance.key < 0) {
+      someInstance.key = 0;
+    }
+    return someInstance.storage[someInstance.key];
   };
 
   someInstance.size = function() {
+    return someInstance.key;
   };
 
   return someInstance;
