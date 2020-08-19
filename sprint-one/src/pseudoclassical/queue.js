5519600ce69166feb4e1b0abc7413064ef6e24c2
chrisalopez1337 <chrisalopez@outlook.com>
2020-08-19 15:51:04 -0700

    Added working psuedoclassical approach for Queue(), passes all tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.i = 0;
+  this.key = 0;
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.i++;
+  this.storage[this.key] = value;
+  this.key++;
+};
 
+Queue.prototype.dequeue = function() {
+  this.i--;
+  if (this.i < 0) {
+    this.i = 0;
+  }
+  var least = Infinity;
+  for (var key in this.storage) {
+    if (key < least) {
+      least = key;
+    }
+  }
+  var string = this.storage[least];
+  delete this.storage[least];
+  return string;
+};
+
+Queue.prototype.size = function() {
+  return this.i;
+};
