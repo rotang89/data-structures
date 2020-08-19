5a92a246679cfb957af896414c44cd4e9e57c0d0
chrisalopez1337 <chrisalopez@outlook.com>
2020-08-19 15:57:57 -0700

    Fixed spacing issues in es6/queue.js

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,7 +2,7 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-    this.i =  0;
+    this.i = 0;
     this.key = 0;
     this.storage = {};
   }
