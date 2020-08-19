da0669d27f4cfa80694a737736ce1abc571e8400
Jonathan Tang <email.john.e@gmail.com>
2020-08-19 15:16:06 -0700

    pseudoclassical queue complete

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.index = 0;
+  this.storage = {};
+
 };
 
+Queue.prototype.size = function() {
+  return this.index;
+};
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.index > 0) {
+    this.index--;
+    let dequeued = this.storage[0];
+    delete this.storage[0];
+    for (var key in this.storage) {
+      this.storage[key - 1] = this.storage[key];
+    }
+    delete this.storage[this.index];
+
+    return dequeued;
+  }
+};
