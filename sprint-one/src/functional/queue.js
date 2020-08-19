7082b8b0d0aa493a676d261a63b92d740b6fd863
Jonathan Tang <email.john.e@gmail.com>
2020-08-19 12:01:43 -0700

    complete functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,13 +6,26 @@ var Queue = function() {
 
   // Implement the methods below
 
+  var index = 0;
+
   someInstance.enqueue = function(value) {
+    storage[index] = value;
+    index++;
   };
 
   someInstance.dequeue = function() {
+    var dequeued = storage[0];
+    delete storage[0];
+    for (var key in storage) {
+      storage[key - 1] = storage[key];
+    }
+    delete storage[index - 1];
+    index--;
+    return dequeued;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,23 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var index = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[index] = value;
+    index++;
   };
 
   someInstance.pop = function() {
+    index--;
+    let popped = storage[index];
+    delete storage[index];
+    return popped;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
