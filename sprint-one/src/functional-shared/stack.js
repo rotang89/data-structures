e2eb480759c244d4a80563ba05b5c429f920e369
chrisalopez1337 <chrisalopez@outlook.com>
2020-08-19 11:47:18 -0700

    General outline working, not passing 2 of the tests, going to refactor later

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.store = {};
+  stack.key = 0;
+  _.extend(stack, stackMethods);
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push(n) {
+    this.store[this.key] = n;
+  },
+  pop() {
+    this.key--;
+    if (this.key < 0) {
+      this.key = 0;
+    }
+    return this.store[this.key];
+  },
+  size() {
+    return this.key;
+  }
+};
 
-
+//Not working on all cases need to refactor.
