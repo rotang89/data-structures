08761c8fcc982d7f6e58d9543134580b249705c1
Jonathan Tang <email.john.e@gmail.com>
2020-08-20 17:47:11 -0700

    tree checkpoint

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,20 +3,32 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
-};
+treeMethods.addChild = function(value) {};
 
-treeMethods.contains = function(target) {
-};
+treeMethods.contains = function(target) {};
+
+//Whenever we add or remove a node, we need to update .childer property.
 
 
+//addChild(value)
+//We have to add the new tree to the children list of its parent
+//Create new tree
+
+//if there are no nodes yet
+//we would init this as the head or primary node
+//else
+//create a new tree(child)
+//add new child tree to parent tree's children array
+
+
+//contains(value)
 
 /*
  * Complexity: What is the time complexity of the above functions?
